2a94cce831765b22716cf5a726e7e7c25b78b4ee
Hugues Tavernier <hugues.tavernier@protonmail.com>
2024-08-30 12:17:54 +0200

    fix font icon for classic theme

--- packages/sandbox/src/main.tsx
+++ packages/sandbox/src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 
 import "maplibre-theme/icons.default.css";
-import "maplibre-theme/extra/legacy.scoped.css";
-import "maplibre-theme/extra/modern.scoped.css";
-import "maplibre-theme/extra/classic.scoped.css";
+import "maplibre-theme/extra/legacy.scoped.compat.css";
+import "maplibre-theme/extra/modern.scoped.compat.css";
+import "maplibre-theme/extra/classic.scoped.compat.css";
 
 import App from "./App.tsx";
 import React from "react";
